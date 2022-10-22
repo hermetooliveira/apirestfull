1815d8af4908d6f455b3a87f02bc893f3798332a
Hermeto Oliveira <hermeto.co3@gmail.com>
2022-10-21 22:58:22 -0300

    commit casa 21102022

--- src/handlers/product.js
+++ src/handlers/product.js
@@ -1,11 +1,20 @@
+const ProductModel = require('../models/product')
+
 const getAll = (request, h) => {
 
     return 'Hello Programador!';
 };
 
-const save = (request, h) => {
+const save = async (req, h) => {
+    const{name, price} = req.payload;
 
-    return 'Oi';
+    const product = new ProductModel;
+    product.name = name;
+    product.price = price;
+    await product.save();
+
+    return h.response(product).code(201);
+    
 };
 
 module.exports = {
